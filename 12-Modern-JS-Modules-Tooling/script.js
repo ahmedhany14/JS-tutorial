04b62f1689979cc9bca78bcc6603574cc0940914
Ahmed Hany <ah7608867@gmail.com>
2024-09-10 20:36:24 +0300

    modules

--- 12-Modern-JS-Modules-Tooling/script.js
+++ 12-Modern-JS-Modules-Tooling/script.js
@@ -1,5 +1,4 @@
 // Importion Module
-
 // Importing single variable
 import { cost } from "./shoppingCard.js";
 console.log("Importing module");
@@ -21,3 +20,26 @@ add("orange", 2);
 import * as ShoppingCart from "./shoppingCard.js";
 ShoppingCart.addToCart("grapes", 10);
 console.log(ShoppingCart.car[0].product);
+
+// Importing default export from module lodash
+
+import cloneDeep from "../node_modules/lodash-es/cloneDeep.js";
+
+const ahmed = {
+  name: "Ahmed",
+  age: 20,
+  address: {
+    city: "Karachi",
+    country: "Pakistan",
+  },
+};
+
+const ahmedTemp = Object.assign({}, ahmed);
+
+const ahmedCopy = cloneDeep(ahmed);
+
+ahmed.address.city = "Lahore";
+
+console.log("Copy", ahmed); // We can see that the address of ahmed object is changed
+console.log("Copy", ahmedTemp); // The data of ahmedTemp is also changed because it is a shallow copy
+console.log("Deep Copy", ahmedCopy); // The data of ahmedCopy is not changed because it is a deep copy
